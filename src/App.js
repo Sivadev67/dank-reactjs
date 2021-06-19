adcf795359d0729c5d1ea6c8fa3315cd000c437a
Sivadev67 <34188538+Sivadev67@users.noreply.github.com>
2021-06-19 18:13:54 +0530

    private app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Edit to see some magic</h2>
+      <h2>made private</h2>
     </div>
   );
 }
